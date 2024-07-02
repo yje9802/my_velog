07723eedd39df36ef55451ed5be5bb991854e642
Ji-eun Yang <jidak1165@gmail.com>
2024-07-02 23:53:04 +0900

    [fix] 버전 수정, 코드 오류 수정

--- scripts/update_velog.py
+++ scripts/update_velog.py
@@ -47,9 +47,9 @@ for entry in feed.entries:
     # 파일이 존재하지 않으면 생성
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as file:
-            contents = f'[원본 링크]({entry.link}) \n <br> \n'
-            contents += entry.description
-            file.write(contents)  # 글 내용을 파일에 작성
+            # contents = f'[원본 링크]({entry.link}) \n <br> \n'
+            # contents += entry.description
+            file.write(entry.description)  # 글 내용을 파일에 작성
             
 
         # 깃허브 커밋

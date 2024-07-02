9040cbd031793f7e764e3250653a9d82d514e61e
Ji-eun Yang <jidak1165@gmail.com>
2024-07-03 00:10:30 +0900

    [fix] 폴더 주소 변경

--- scripts/update_velog.py
+++ scripts/update_velog.py
@@ -37,24 +37,23 @@ for entry in feed.entries:
     else:
         folder_name = '[ETC.]'
     
-    if not os.path.exists(folder_name):
-        os.makedirs(folder_name)
     specified_posts_dir = posts_dir + '/' + folder_name
+    if not os.path.exists(specified_posts_dir):
+        os.makedirs(specified_posts_dir)
     
     file_name += '.md'
     file_path = os.path.join(specified_posts_dir, file_name)
 
     # 파일이 존재하지 않으면 생성
     if not os.path.exists(file_path):
-        print(file_path)
-        # with open(file_path, 'w', encoding='utf-8') as file:
+        with open(file_path, 'w', encoding='utf-8') as file:
             # contents = f'[원본 링크]({entry.link}) \n <br> \n'
             # contents += entry.description
-            # file.write(entry.description)  # 글 내용을 파일에 작성
+            file.write(entry.description)  # 글 내용을 파일에 작성
             
 
         # 깃허브 커밋
-        # repo.git.add(file_path)
+        repo.git.add(file_path)
         repo.git.commit('-m', f'포스트 업데이트: {entry.title}')
 
 # 변경 사항을 깃허브에 푸시
